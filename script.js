b1a3e9ded2de74f630f92daf366125473960ea25
lucasgalindo <lucasgalindomds@gmail.com>
2025-05-12 14:15:12 -0300

    fix usuário só consegue digitar - Pedra, papel ou tesoura -

--- script.js
+++ script.js
@@ -1,61 +1,66 @@
-function getComputerChoice() {
+ function getComputerChoice() {
 
-    const choices = ["Pedra", "Papel", "Tesoura"];
-    const randomIndex = Math.floor(Math.random() * choices.length);
-    return choices[randomIndex];
+            const choices = ["Pedra", "Papel", "Tesoura"];
+            const randomIndex = Math.floor(Math.random() * choices.length);
+            return choices[randomIndex];
 
-}
-
-function getHumanChoice() {
-
-    let choice = prompt("Digite sua escolha (Pedra, papel ou tesoura): ");
-    return choice[0].toUpperCase() + choice.slice(1).toLowerCase();
-
-}
-
-function playGame() {
-
-    let i = 1;
-    let humanScore = 0;
-    let computerScore = 0;
-
-    while (i <= 5) {
-
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-
-        if (humanSelection == computerSelection) {
-            console.log("Deu empate.");
         }
 
-        else if (humanSelection == "Pedra" && computerSelection == "Tesoura") {
-            console.log("Humano ganhou.");
-            humanScore++;
+        function getHumanChoice() {
+
+            let choice = prompt("Digite sua escolha (Pedra, papel ou tesoura): ");
+            choice = choice[0].toUpperCase() + choice.slice(1).toLowerCase();
+            if (choice === "Pedra" || choice === "Tesoura" || choice === "Papel") {
+                return choice;
+            } else {
+                alert("Escolha inválida, tente novamente");
+                return getHumanChoice();
+            }
         }
 
-        else if (humanSelection == "Tesoura" && computerSelection == "Papel") {
-            console.log("Humano ganhou.");
-            humanScore++;
+        function playGame() {
+
+            let i = 1;
+            let humanScore = 0;
+            let computerScore = 0;
+
+            while (i <= 5) {
+
+                const humanSelection = getHumanChoice();
+                const computerSelection = getComputerChoice();
+
+                if (humanSelection == computerSelection) {
+                    console.log("Deu empate.");
+                }
+
+                else if (humanSelection == "Pedra" && computerSelection == "Tesoura") {
+                    console.log("Humano ganhou.");
+                    humanScore++;
+                }
+
+                else if (humanSelection == "Tesoura" && computerSelection == "Papel") {
+                    console.log("Humano ganhou.");
+                    humanScore++;
+                }
+
+                else if (humanSelection == "Papel" && computerSelection == "Pedra") {
+                    console.log("Humano ganhou.");
+                    humanScore++;
+                } else {
+                    console.log("Computador ganhou");
+                    computerScore++;
+                }
+
+                i++;
+            }
+
+            if (humanScore > computerScore) {
+                console.log("Humano venceu a disputa");
+            } else if (computerScore > humanScore) {
+                console.log("Pc venceu a disputa");
+            } else {
+                console.log("Deu empate.");
+            }
         }
 
-        else if (humanSelection == "Papel" && computerSelection == "Pedra") {
-            console.log("Humano ganhou.");
-            humanScore++;
-        } else {
-            console.log("Computador ganhou");
-            computerScore++;
-        }
-
-        i++;
-    }
-
-    if (humanScore > computerScore) {
-        console.log("Humano venceu a disputa");
-    } else if (computerScore > humanScore) {
-        console.log("Pc venceu a disputa");
-    } else {
-        console.log("Deu empate.");
-    }
-}
-
-playGame();
+        playGame();
